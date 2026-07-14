cf65762c48730a9eee93c840bd8afaddd67aa74b
j4728223-pixel <j4728223@gmail.com>
2026-07-14 11:37:41 +0500

    Add: Bo'lish turlarini farqlash (/ va //) [j4728223-pixel]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/15-Xatolik_turlari_____syntax_vs_runtime__xatoni_o_qi/01-Bo_lish_turlarini_farqlash____va____.py
@@ -0,0 +1,9 @@
+# Kirish ma'lumotlarini o'qish
+a = int(input())
+b = int(input())
+
+# 1 - qator: oddiy bo'lish (float natija)
+print(a / b)
+
+# 2 - qator: butun bo'lish (integer natija)
+print(a // b)
