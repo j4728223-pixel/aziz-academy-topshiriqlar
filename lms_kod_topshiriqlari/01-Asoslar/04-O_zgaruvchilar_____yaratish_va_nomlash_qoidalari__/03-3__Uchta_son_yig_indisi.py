35396029263f5b0de50eb10a872b76092c62a5f4
j4728223-pixel <j4728223@gmail.com>
2026-07-25 12:02:05 +0500

    Add: 3) Uchta son yig'indisi [j4728223-pixel]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/03-3__Uchta_son_yig_indisi.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/03-3__Uchta_son_yig_indisi.py
@@ -1,5 +1,5 @@
-a, b, c = map(int, input().split())
-
-yigindi = a + b + c
-
-print(f"Yig'indi: {yigindi}")
+a, b, c =map(int, input().split())
+summa = a + b + c 
+print("Yig'indi:", summa)
+# Bir qatorda uchta butun son kiriting (probel bilan).
+# Ularning yig'indisini hisoblab, "Yig'indi: <summa>" ko'rinishida chiqaring.
