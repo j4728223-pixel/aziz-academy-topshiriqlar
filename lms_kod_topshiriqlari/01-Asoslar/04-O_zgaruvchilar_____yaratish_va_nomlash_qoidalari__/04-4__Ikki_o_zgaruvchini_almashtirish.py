1651ee839b86d62a7f5e41230e914b8643a464c6
j4728223-pixel <j4728223@gmail.com>
2026-07-25 12:02:36 +0500

    Add: 4) Ikki o‘zgaruvchini almashtirish [j4728223-pixel]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/04-4__Ikki_o_zgaruvchini_almashtirish.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/04-4__Ikki_o_zgaruvchini_almashtirish.py
@@ -1,6 +1,8 @@
-a,b = map(int, input().split())
-
+a, b =map(int, input().split())
 a, b = b, a
-
 print(f"a={a}")
 print(f"b={b}")
+# Bir qatorda ikki butun son kiriting: a va b.
+# Ularning qiymatini o'zaro almashtiring va quyidagicha chiqaring:
+# a=<yangi_a>
+# b=<yangi_b>
