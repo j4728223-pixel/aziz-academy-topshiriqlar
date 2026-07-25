138efbd20e39a7441d50f4a10f367bfa343a1748
j4728223-pixel <j4728223@gmail.com>
2026-07-25 12:03:04 +0500

    Add: 10) Turli tipdagi o‘zgaruvchilar [j4728223-pixel]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/10-10__Turli_tipdagi_o_zgaruvchilar.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/10-10__Turli_tipdagi_o_zgaruvchilar.py
@@ -1,6 +1,8 @@
+# 1-qator: ism
+# 2-qator: yosh (butun son)
+# "<ism> <yosh> yoshda" ko'rinishida satr hosil qilib, uni chiqaring.
+# O'zgaruvchi nomlarini mazmunli qiling (masalan: name, age, message).
 name = input()
-age = int(input())
-
-message = f"{name} {age} yoshda"
-
+age = input()
+message = name + " " + age + " yoshda "
 print(message)
