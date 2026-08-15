287e9dc520b2ea50c9d857860a88b57da96a050d
j4728223-pixel <j4728223@gmail.com>
2026-08-15 11:02:23 +0500

    Add AI task: Sport zali: chegirma hisobi

---
+++ ai_topshiriqlar/Sport_zali_chegirma_hisobi.py
@@ -0,0 +1,15 @@
+# Sport zali: chegirma hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = int(input())
+b = int(input())
+
+total = (a + b) * 0.9
+
+if total < 100000:
+    total += 10000
+    
+    print(int(total))
